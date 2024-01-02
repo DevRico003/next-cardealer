0724287b658fffb135b9c99bbbfc20515459100f
Enrico Carteciano <carteciano.enrico@gmail.com>
2024-01-02 09:47:26 +0100

    reactivated preloader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
     // Simulate loading for 3 seconds (adjust as needed)
     setTimeout(() => {
       setLoading(false);
-    }, 0);
+    }, 100);
   }, []);
   useEffect(() => {
     import("../../public/assets/js/bootstrap.min.js");
