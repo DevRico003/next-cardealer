1d37346888c8450caf11de694e4feaa2dee04097
Enrico Carteciano <carteciano.enrico@gmail.com>
2024-01-02 10:27:41 +0100

    preloader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
     // Simulate loading for 3 seconds (adjust as needed)
     setTimeout(() => {
       setLoading(false);
-    }, 3000);
+    }, 500);
   }, []);
   useEffect(() => {
     import("../../public/assets/js/bootstrap.min.js");
@@ -39,7 +39,7 @@ function MyApp({ Component, pageProps }) {
               sizes="20x20"
             />
           </Head>
-          {/* <Preloader onClose={handlePreloaderClose} /> */}
+          <Preloader onClose={handlePreloaderClose} />
         </>
       ) : (
         <>
